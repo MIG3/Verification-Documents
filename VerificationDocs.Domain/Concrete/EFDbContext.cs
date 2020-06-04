4828a9f3663c29ff29a2234151d660a7bca81b33
MIG3 <35615450+MIG3@users.noreply.github.com>
2020-06-04 12:27:07 +0300

    + контекст для связи с таблицей безопасности

--- VerificationDocs.Domain/Concrete/EFDbContext.cs
+++ VerificationDocs.Domain/Concrete/EFDbContext.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
 using VerificationDocs.Domain.Entities;
+using IdentityModel;
 
 namespace VerificationDocs.Domain.Concrete
 {
@@ -14,5 +15,13 @@ namespace VerificationDocs.Domain.Concrete
         // Имя свойства указывает таблицу, а параметр типа результата DbSet - тип модели, 
         // который инфраструктура Entity Framework должна использовать для представления строк в этой таблице
         public DbSet<StyleP> Style { get; set; }
+    }
+    public class SecurityContext : DbContext
+    {
+        public SecurityContext() :
+            base("EFDbContext")
+        { }
+
+        public DbSet<Security> UsersSec { get; set; }
     }
 }
