4d6a7e046926c9d73bc8d7060bf7a92f111bb48e
MIG3 <35615450+MIG3@users.noreply.github.com>
2020-06-04 12:40:38 +0300

    Определён конструктор

---
+++ VerificationDocs.WebUI/Controllers/DocumentStatusController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VerificationDocs.Domain.Abstract;
+
+namespace VerificationDocs.WebUI.Controllers
+{
+    public class DocumentStatusController : Controller
+    {
+        // GET: DocumentStatus
+        private IDocumentStatusRepository repository;
+
+        public DocumentStatusController(IDocumentStatusRepository repo)
+        {
+            repository = repo;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
