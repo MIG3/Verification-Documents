41c92d66e2fe4387061b49e34ca19d1b2eeab820
MIG3 <35615450+MIG3@users.noreply.github.com>
2020-06-04 12:50:41 +0300

    Добавлена модель регистрации

---
+++ VerificationDocs.WebUI/Models/Models.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using VerificationDocs.Domain.Entities;
+using VerificationDocs.Domain.Concrete;
+
+namespace VerificationDocs.WebUI.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Login { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Login { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+    }
+}
